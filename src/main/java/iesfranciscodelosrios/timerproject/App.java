702800901537b49539b71e6dca6f365f72cb125d
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-11-22 17:23:50 +0100

    CasiTerminado(DetenerYReanudarNoFuncionan)

--- src/main/java/iesfranciscodelosrios/timerproject/App.java
+++ src/main/java/iesfranciscodelosrios/timerproject/App.java
@@ -7,12 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("primary.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 705, 260);
         stage.setScene(scene);
         stage.show();
     }
